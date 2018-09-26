d78966d4c11f7e3eb21a7f9ffb25046ae0d7c8a2
firebird422 <somekenten@gmail.com>
2018-09-25 22:09:45 -0700

    Added different state changes in App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Lily', age: 12},
+      { name: 'Raz', age: 13},
+      { name: 'Holly', age: 4}
+    ],
+    someOtherState: 'Some Value'
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was clicked');
+    //Don't do this this.state.persons[0].name = 'Sweet Kitty';
+    this.setState( {
+      persons: [
+        { name: 'Sweet Kitty', age: 12},
+        { name: 'Raz', age: 13},
+        { name: 'Holly', age: 5}
+      ]
+    } )
+  }
+
   render() {
     return (
       <div className="App">
       <h1>Ihi, I'm a react app</h1>
-      <Person name="Lily" age="12"/>
-      <Person name="Raz" age="13">Hobbies: Eating</Person>
-      <Person name="Holly" age="4"/>
+      <button onClick={this.switchNameHandler}>Switch Name</button>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Hobbies: Eating</Person>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
